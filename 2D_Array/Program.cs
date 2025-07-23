9ee5854d85a8872ef1fabab50773060149f3dbe2
Birendra <birendrakalauni33@gmail.com>
2025-07-23 15:25:59 +0545

    Updated code

--- 2D_Array/Program.cs
+++ 2D_Array/Program.cs
@@ -1,34 +1,73 @@
 ﻿// See https://aka.ms/new-console-template for more informat
 
+// using System;
+
+// namespace matrix
+// {
+//     class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             // int[,] array = new int[3, 3];
+//             // int i, j;
+//             // Console.WriteLine("Enter elements of 3x3 matrix:");
+
+//             // for (i = 0; i < 3; i++)
+//             // {
+//             //     for (j = 0; j < 3; j++)
+//             //     {
+//             //         Console.Write($"Element [{i},{j}]: ");
+//             //         array[i, j] = Convert.ToInt32(Console.ReadLine());
+//             //     }
+//             // }
+
+//             // Console.WriteLine("\nThe Matrix is:");
+//             // for (i = 0; i < 3; i++)
+//             // {
+//             //     for (j = 0; j < 3; j++)
+//             //     {
+//             //         Console.Write(array[i, j] + "\t");
+//             //     }
+//             //     Console.WriteLine(); // for new row
+//             // }
+
+//             //Access Elements of a 2D Array
+//             int[,] Num = { { 9, 8, 5 }, { 4, 7, 3 } };
+//             Console.WriteLine(Num[2, 2]);
+//         }
+//     }
+// }
+
 using System;
 
-namespace matrix
+namespace Array
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int[,] array = new int[3, 3];
-            int i, j;
-            Console.WriteLine("Enter elements of 3x3 matrix:");
+            int[,] Num = { { 9, 8, 5 }, { 4, 7, 3 } };
+            Console.WriteLine(Num[1, 2]);
 
-            for (i = 0; i < 3; i++)
+            //Change Elements of a 2D Array
+            Console.WriteLine("Value Change:");
+            Num[0, 0] = 1;//Change value to 5
+            Console.WriteLine(Num[0, 0]);
+
+            //Loop Through a 2D Array
+            Console.WriteLine("Using Loop:");
+            Console.WriteLine("Using foreach loop:");
+            foreach (int i in Num)//foreach loop
             {
-                for (j = 0; j < 3; j++)
-                {
-                    Console.Write($"Element [{i},{j}]: ");
-                    array[i, j] = Convert.ToInt32(Console.ReadLine());
-                }
+                Console.WriteLine(i);
             }
-
-            Console.WriteLine("\nThe Matrix is:");
-            for (i = 0; i < 3; i++)
+            Console.WriteLine("Using for loop:");
+            for (int i = 0; i < Num.GetLength(0); i++)
             {
-                for (j = 0; j < 3; j++)
+                for (int j = 0; j < Num.GetLength(1); j++)
                 {
-                    Console.Write(array[i, j] + "\t");
+                    Console.WriteLine(Num[i, j]);
                 }
-                Console.WriteLine(); // for new row
             }
         }
     }
--- 2D_Array/obj/Debug/net9.0/2D_Array.AssemblyInfo.cs
+++ 2D_Array/obj/Debug/net9.0/2D_Array.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("2D_Array")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+414306565c802994064c5954fe68613941807a40")]
 [assembly: System.Reflection.AssemblyProductAttribute("2D_Array")]
 [assembly: System.Reflection.AssemblyTitleAttribute("2D_Array")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
