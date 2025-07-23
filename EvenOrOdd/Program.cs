b86f31cc0f39e34d5818e29ccaa56c227dba31c1
Birendra <birendrakalauni33@gmail.com>
2025-07-23 13:44:10 +0545

    Added EvenOrOdd and Jagged Array project

---
+++ EvenOrOdd/Program.cs
@@ -0,0 +1,43 @@
+﻿// Program for printing even or odd
+using System;
+
+namespace EvenOrOdd
+{
+    class EvenOrOdd
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Do you want to print Even or Odd numbers? (E/O):");
+            char choice = Convert.ToChar(Console.ReadLine().ToUpper());
+
+            if (choice == 'E')
+            {
+                Console.WriteLine("Even numbers from 0 to 100:");
+                for (int i = 0; i <= 100; i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+            }
+            else if (choice == 'O')
+            {
+                Console.WriteLine("Odd numbers from 0 to 100:");
+                int j = 0;
+                while (j <= 100)
+                {
+                    if (j % 2 != 0)
+                    {
+                        Console.WriteLine(j);
+                    }
+                    j++;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input! Please enter 'E' or 'O'.");
+            }
+        }
+    }
+}
---
+++ Jagged_Array/Program.cs
@@ -0,0 +1,27 @@
+﻿// C# Jagged Arrays
+using System;
+
+namespace JaggedArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // // Declare a jagged array with 3 sub-arrays
+            // int[][] jarray = new int[3][];
+
+            // // Initialize sub-arrays
+            // jarray[0] = new int[] { 1, 2, 3 };
+            // jarray[1] = new int[] { 4, 5 };
+            // jarray[2] = new int[] { 6 };
+
+            // // Print the contents of the first sub-array
+            // Console.WriteLine(string.Join(", ", jarray[0]));
+            int[][] jarray = new int[3][];
+            jarray[0] = new int[5] { 1, 2, 3, 4, 5 };
+            jarray[1] = new int[3] { 10, 20, 30 };
+            jarray[2] = new int[] { 12, 50, 60, 70, 32 };
+            Console.WriteLine("The first element of first row:" + jarray[0][0]);
+        }
+    }
+}
