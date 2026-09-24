68fa43e9d167ee9e34fdce644c2f8037bf21ccef
Birendra Kalauni <birendrakalauni33@gmail.com>
2026-09-24 17:33:33 +0545

    Add C# properties example with validation

---
+++ Properties/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace C_.OOP;
+
+public class Student
+{
+    private int age; //field
+    
+    //Property
+    public int Age
+    {
+        get
+        {
+            return age;
+        }
+        set
+        {
+            if(value >= 0)
+            {
+                age = value;
+            }
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        //Creating object
+        Student student =new Student();
+
+        //Set age using the property
+        student.Age =22;
+
+        Console.WriteLine("Student Age: "+ student.Age); //Get age using the property
+
+        //Try to set an invalid age
+        student.Age =-5;
+        Console.WriteLine("Student Age after invalid value: "+ student.Age);
+    }
+}
