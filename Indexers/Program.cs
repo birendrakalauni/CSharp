bb36157b8288fb252a5947f74cecf943bd7bac97
Birendra Kalauni <birendrakalauni33@gmail.com>
2026-09-25 22:58:00 +0545

    Add indexer example

---
+++ Indexers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class StudentCollection
+{
+    private string[] students = new string[3];
+    public string this[int index]
+    {
+        get
+        {
+            return students[index];
+        }
+        set
+        {
+             students[index]=value;
+        }
+    }
+}
+ class Program
+    {
+        static void Main()
+    {
+        StudentCollection s = new StudentCollection();
+        s[0] = "Ram";
+        s[1]= "Shyam";
+        Console.WriteLine(s[0]);
+        Console.WriteLine(s[1]);
+    }
+    }
